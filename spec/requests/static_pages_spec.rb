3072703ad3eac0843ba4a79fd8ab34b27ea598bb
riartoma <ratmanri@gmail.com>
2013-10-25 12:14:01 -0500

    agregar el helper full

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,19 +1,32 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+
+describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
+
+
+
+
+
 
   describe "Help page" do
 
