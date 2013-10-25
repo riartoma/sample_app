9b043a90c5263580686cb7573878db3895475074
riartoma <ratmanri@gmail.com>
2013-10-25 00:26:05 -0500

    hasta aca se crea una nueva vista siguiendo los pasos mencionados y creando los archivos y modificaciones necesarias de los archivos existentes

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+end
