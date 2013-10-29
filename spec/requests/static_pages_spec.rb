9d0408362e2b0eaa2e5a329783190e80212cffdc
riartoma <ratmanri@gmail.com>
2013-10-29 10:37:44 -0500

    terminando el cap 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,19 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
-  subject { page }
-
-  describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
-  end
-
-  describe "Help page" do
-    before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
-  end
-
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to # fill in
+    click_link "Contact"
+    expect(page).to # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to # fill in
+    click_link "sample app"
+    expect(page).to # fill in
   end
 end
