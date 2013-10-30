dc23a9cddd862147e13f382ef47cfaf75249ed68
riartoma <ratmanri@gmail.com>
2013-10-30 18:20:41 -0500

    arreglando rutas de css para el servidor local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,12 +13,12 @@ SampleApp::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-  config.serve_static_assets = true
+  
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
   # Compress JavaScripts and CSS.
@@ -26,7 +26,7 @@ SampleApp::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true #false
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
