97ad654352ebb67cf02c4e027801c15f8fedbf00
riartoma <ratmanri@gmail.com>
2013-11-04 23:39:10 -0500

    ingreso de usuarios terminado

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,11 +19,29 @@ describe "User pages" do
     it { should have_title(full_title('Sign up')) }
   end
 
-  describe "profile page" do
-  	# Replace with code to make a user variable
-  	before { visit user_path(user) }
+  describe "signup" do
 
-  	it { should have_content(user.name) }
-  	it { should have_title(user.name) }
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
