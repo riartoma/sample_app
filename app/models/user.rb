9b2eaa4fd966f21dfde9d717123457d9c7add0ed
riartoma <ratmanri@gmail.com>
2013-11-04 19:13:51 -0500

    validando emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  before_save { email.downcase! }
 end
