a9a580816e1b84b58929e0377002937c123a9d03
riartoma <ratmanri@gmail.com>
2013-11-05 13:40:37 -0500

    organizando las vistas e instanciando

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -110,6 +110,19 @@ describe "User pages" do
   end
 
   describe "edit" do
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
+    end
+
     let(:user) { FactoryGirl.create(:user) }
     before do
       sign_in user
