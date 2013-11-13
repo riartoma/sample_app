f4f192363027424be588aaf8d9f130e15343b9f8
riartoma <ratmanri@gmail.com>
2013-11-13 01:39:14 -0500

    assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ SampleApp::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = true
-  config.assets.precompile = true
+  config.assets.precompile = [/^[-_a-zA-Z0-9]\../]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
